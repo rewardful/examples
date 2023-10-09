de4641374524482e3195d9f701972bd20508c45b
Jordi Noguera <jordinoguera83@gmail.com>
2023-10-09 18:23:54 +0200

    Add Next (TS) app router example

---
+++ next-ts-app-router/app/layout.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/script'
+
+// Use your Rewardful API Key
+const API_KEY= process.env.NEXT_PUBLIC_REWARDFUL_DEMO_API_KEY
+
+// If not setting NEXT_PUBLIC_APP_REWARDFUL_SCRIPT_URL, just use https://r.wdfl.co/rw.js
+const SCRIPT_URL = process.env.NEXT_PUBLIC_APP_REWARDFUL_SCRIPT_URL || 'https://r.wdfl.co/rw.js'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Script src={SCRIPT_URL} data-rewardful={API_KEY}></Script>
+        <Script id="rewardful-queue" strategy="beforeInteractive">
+            {`(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`}
+        </Script>
+      </body>
+    </html>
+  )
+}
---
+++ next-ts-app-router/app/page.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import Script from 'next/script'
+
+// Use your own Stripe Publishable Key (THIS IS NOT A STRIPE API KEY!!)
+const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_APP_STRIPE_PUBLISHABLE_KEY
+
+// Use your own Stripe ID
+const STRIPE_PRICE_ID = process.env.NEXT_PUBLIC_APP_PRICE_ID
+
+export default function PreviewPage() {
+    const onClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
+        const stripe = (window as any).Stripe(STRIPE_PUBLISHABLE_KEY);
+
+        stripe.redirectToCheckout({
+            lineItems: [{ price: STRIPE_PRICE_ID, quantity: 1 }],
+            mode: 'payment',
+            successUrl: `${window.location.origin}?result=success`,
+            cancelUrl: `${window.location.origin}?result=canceled`,
+            clientReferenceId: (window as any).Rewardful.referral
+        })
+    }
+
+    return (
+        <>
+            <form onClick={onClick}>
+                <section>
+                    <button type="submit" role="link">
+                        Checkout
+                    </button>
+                </section>
+                <style jsx>
+                    {`
+                      section {
+                        background: #ffffff;
+                        display: flex;
+                        flex-direction: column;
+                        width: 400px;
+                        height: 112px;
+                        border-radius: 6px;
+                        justify-content: space-between;
+                      }
+
+                      button {
+                        height: 36px;
+                        background: #556cd6;
+                        border-radius: 4px;
+                        color: white;
+                        border: 0;
+                        font-weight: 600;
+                        cursor: pointer;
+                        transition: all 0.2s ease;
+                        box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+                      }
+
+                      button:hover {
+                        opacity: 0.8;
+                      }
+                    `}
+                </style>
+            </form>
+            <Script src="https://js.stripe.com/v3"></Script>
+        </>
+    );
+}
